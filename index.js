45d30dc2fe8863d68960aead6e575fbcbc017d41
Arno Schutijzer <arno.schutijzer@gmail.com>
2019-09-22 18:57:03 +0200

    feat: display response errors if applicable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 const Unbg = require('./lib/unbg');
+const pkg = require('./package.json');
 
 async function run() {
   try {
@@ -12,7 +13,14 @@ async function run() {
     console.log(`setting ${path}`);
     await unbg.setBackground(path);
   } catch(error) {
-    console.log(error);
+    console.log(`Oops, something went wrong. Have a look at the error(s) below or open an issue at ${pkg.bugs.url}`)
+    if (error.response && error.response && error.response.data && error.response.data.errors) {
+      error.response.data.errors.forEach((errorMessage) => {
+        console.log(errorMessage);
+      });
+    } else {
+      console.log(error);
+    }
     return process.exit(1);
   }
 }
